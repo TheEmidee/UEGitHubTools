de976e75c37c2dabe7ebc9e5d27936d5b6938ffb
Michael Delva <michael@emidee.net>
2024-06-05 23:09:03 +0200

    Moved function implementation in cpp

--- Source/GitHubTools/Private/GitHubToolsHttpRequest.h
+++ Source/GitHubTools/Private/GitHubToolsHttpRequest.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "GitHubToolsSettings.h"
 #include "GitHubToolsTypes.h"
-#include "HttpModule.h"
 
 #include <CoreMinimal.h>
 #include <Interfaces/IHttpRequest.h>
@@ -55,7 +53,7 @@ public:
         Request( Forward< TArgTypes >( args )... )
     {}
 
-    virtual ~TGitHubToolsHttpRequest() = default;
+    virtual ~TGitHubToolsHttpRequest() override = default;
 
     bool ProcessRequest();
 
@@ -75,68 +73,6 @@ protected:
     TPromise< TResponse > Promise;
     TRequest Request;
 };
-
-template < typename TRequest, typename TResponse >
-bool TGitHubToolsHttpRequest< TRequest, TResponse >::ProcessRequest()
-{
-    FString token;
-    FString repository_owner;
-    FString repository_name;
-
-    if ( auto * settings = GetDefault< UGitHubToolsSettings >() )
-    {
-        token = settings->Token;
-        repository_owner = settings->RepositoryOwner;
-        repository_name = settings->RepositoryName;
-    }
-
-    if ( token.IsEmpty() )
-    {
-        return false;
-    }
-
-    if ( repository_owner.IsEmpty() )
-    {
-        return false;
-    }
-
-    if ( repository_name.IsEmpty() )
-    {
-        return false;
-    }
-
-    const auto request = FHttpModule::Get().CreateRequest();
-
-    static const FString verbs[] = {
-        TEXT( "GET" ),
-        TEXT( "POST" )
-    };
-
-    request->SetVerb( verbs[ static_cast< uint8 >( Request.GetVerb() ) ] );
-    request->SetHeader( TEXT( "Accept" ), TEXT( "application/json" ) );
-    request->SetHeader( TEXT( "Content-Type" ), TEXT( "application/vnd.github+json" ) );
-
-    TStringBuilder< 128 > token_builder;
-    token_builder << TEXT( "Bearer " );
-    token_builder << token;
-
-    request->SetHeader( TEXT( "Authorization" ), *token_builder );
-    request->SetHeader( TEXT( "X-GitHub-Api-Version" ), TEXT( "2022-11-28" ) );
-
-    TStringBuilder< 256 > string_builder;
-    string_builder << TEXT( "https://api.github.com/repos/" );
-    string_builder << repository_owner;
-    string_builder << TEXT( "/" );
-    string_builder << repository_name;
-    string_builder << TEXT( "/" );
-    string_builder << Request.GetEndPoint();
-
-    request->SetURL( *string_builder );
-    request->SetContentAsString( Request.GetBody() );
-    request->OnProcessRequestComplete().BindRaw( this, &::TGitHubToolsHttpRequest< TRequest, TResponse >::OnProcessRequestComplete );
-
-    return request->ProcessRequest();
-}
 
 class FGitHubToolsHttpRequestManager : public TSharedFromThis< FGitHubToolsHttpRequestManager >
 {
